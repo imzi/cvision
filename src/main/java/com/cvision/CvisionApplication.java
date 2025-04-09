9b9cdf1e521490a158609a711505fae75c786d09
imzi <amimthath@gmail.com>
2025-04-09 20:28:22 +0530

    initial project setup

---
+++ src/main/java/com/cvision/CvisionApplication.java
@@ -0,0 +1,13 @@
+package com.cvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvision/CvisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
